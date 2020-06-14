814f268ba50adf273718accef897e8c5944cbaa0
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-14 05:06:15 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,9 +3,9 @@ def find_element_index(array, value_to_find)
   value = 0
   
   while value < array.length do
-  if array[value] == value_to_find
-  return value
-  end
-  value += 1
+    if array[value] == value_to_find
+    return value
+    end
+    value += 1
   end 
 end
