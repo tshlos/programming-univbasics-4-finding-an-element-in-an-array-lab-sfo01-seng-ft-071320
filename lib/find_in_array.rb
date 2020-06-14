f543efea53885952381b1e1c1cdd27c702e8ee5f
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-14 05:02:28 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,12 +1,3 @@
 def find_element_index(array, value_to_find)
   
-  index = 0
-  result = 0
-  count = 0
-  
-  if (array[index] == value_to_find)
-    result = value_to_find
-    count += 1 
-  end
-  result[index]
 end
